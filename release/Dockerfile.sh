e3256f61f995c9f0737a7ade56d79873b2d80889
Lars Uebernickel <lars@uebernic.de>
2016-03-22 14:52:22 +0100

    release: seed pbuilder with build dependencies

--- release/Dockerfile.sh
+++ release/Dockerfile.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 # Certain commands cannot be run in Dockerfile including pbuilder run them here
-sudo pbuilder create --distribution unstable
+
+BUILD_DEPENDS="debhelper dh-autoreconf autoconf automake intltool libssh-dev libssl-dev zlib1g-dev libkrb5-dev
+               libxslt1-dev libkeyutils-dev libglib2.0-dev libsystemd-dev libpolkit-agent-1-dev libpcp3-dev
+               libjson-glib-dev libpam0g-dev libpcp-import1-dev libpcp-pmda3-dev xsltproc xmlto docbook-xsl
+               glib-networking nodejs-legacy npm openssh-client"
+
+sudo pbuilder create --distribution unstable --extrapackages "$BUILD_DEPENDS"
