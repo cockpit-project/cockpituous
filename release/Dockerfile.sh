81762472ca301348f7464c4885930185f06e2194
Stef Walter <stefw@redhat.com>
2016-12-15 11:17:39 +0100

    relrease: Split out dependencies for Jessie and Unstable

--- release/Dockerfile.sh
+++ release/Dockerfile.sh
@@ -2,10 +2,17 @@
 
 # Certain commands cannot be run in Dockerfile including pbuilder run them here
 
-BUILD_DEPENDS="debhelper dh-autoreconf autoconf automake intltool libssh-dev libssl-dev zlib1g-dev libkrb5-dev
-               libxslt1-dev libkeyutils-dev libglib2.0-dev libsystemd-dev libpolkit-agent-1-dev libpcp3-dev
-               libjson-glib-dev libpam0g-dev libpcp-import1-dev libpcp-pmda3-dev xsltproc xmlto docbook-xsl
-               glib-networking nodejs-legacy npm openssh-client"
+DEPENDS="debhelper dh-autoreconf autoconf automake intltool zlib1g-dev libkrb5-dev
+         libxslt1-dev libkeyutils-dev libglib2.0-dev libsystemd-dev libpolkit-agent-1-dev libpcp3-dev
+         libjson-glib-dev libpam0g-dev libpcp-import1-dev libpcp-pmda3-dev xsltproc xmlto docbook-xsl
+         glib-networking npm openssh-client"
 
-sudo pbuilder create --distribution jessie --basetgz /var/cache/pbuilder/jessie.tgz --extrapackages "$BUILD_DEPENDS"
-sudo pbuilder create --distribution unstable --basetgz /var/cache/pbuilder/unstable.tgz --extrapackages "$BUILD_DEPENDS"
+JESSIE_DEPENDS="$DEPENDS nodejs libssh-dev"
+
+sudo pbuilder create --distribution jessie --basetgz /var/cache/pbuilder/jessie.tgz \
+    --extrapackages "$JESSIE_DEPENDS"
+
+UNSTABLE_DEPENDS="$DEPENDS nodejs-legacy libssh-dev libssl-dev"
+
+# sudo pbuilder create --distribution unstable --basetgz /var/cache/pbuilder/unstable.tgz \
+#     --extrapackages "$UNSTABLE_DEPENDS"
