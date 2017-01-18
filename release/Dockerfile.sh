fcf8fab05237de835b53eef58b0416c1385f5ef1
Stef Walter <stefw@redhat.com>
2017-01-18 12:38:58 +0100

    Rebuild debian pbuilders and update dependencies

--- release/Dockerfile.sh
+++ release/Dockerfile.sh
@@ -2,17 +2,17 @@
 
 # Certain commands cannot be run in Dockerfile including pbuilder run them here
 
-DEPENDS="debhelper dh-autoreconf autoconf automake intltool zlib1g-dev libkrb5-dev
-         libxslt1-dev libkeyutils-dev libglib2.0-dev libsystemd-dev libpolkit-agent-1-dev libpcp3-dev
-         libjson-glib-dev libpam0g-dev libpcp-import1-dev libpcp-pmda3-dev xsltproc xmlto docbook-xsl
-         glib-networking npm openssh-client"
+DEPENDS="debhelper dpkg-dev dh-autoreconf autoconf automake intltool zlib1g-dev libkrb5-dev
+         libxslt1-dev libkeyutils-dev libglib2.0-dev libsystemd-dev libpolkit-agent-1-dev
+         libjson-glib-dev libpam0g-dev xsltproc xmlto docbook-xsl
+         glib-networking openssh-client libssh-dev"
 
-JESSIE_DEPENDS="$DEPENDS nodejs libssh-dev"
+JESSIE_DEPENDS="$DEPENDS  libpcp3-dev libpcp-import1-dev libpcp-pmda3-dev"
 
-sudo pbuilder create --distribution jessie --basetgz /var/cache/pbuilder/jessie.tgz \
-    --extrapackages "$JESSIE_DEPENDS"
+sudo pbuilder create --distribution jessie --mirror http://deb.debian.org/debian \
+    --basetgz /var/cache/pbuilder/jessie.tgz --extrapackages "$JESSIE_DEPENDS"
 
-UNSTABLE_DEPENDS="$DEPENDS nodejs-legacy libssh-dev"
+UNSTABLE_DEPENDS="$DEPENDS"
 
-sudo pbuilder create --distribution unstable --basetgz /var/cache/pbuilder/unstable.tgz \
-     --extrapackages "$UNSTABLE_DEPENDS"
+sudo pbuilder create --distribution unstable --mirror http://deb.debian.org/debian \
+    --basetgz /var/cache/pbuilder/unstable.tgz --extrapackages "$UNSTABLE_DEPENDS"
