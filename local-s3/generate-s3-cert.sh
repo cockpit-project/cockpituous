026e08b73cc951be73bab7b29ad46d078a301dc8
Martin Pitt <mpitt@redhat.com>
2022-11-25 17:54:45 +0100

    images: Move S3 server related resources into local-s3/
    The local S3 server had nothing to do with the old cockpit/images
    containers, they just happened to share the same SSL certificate.
    
    As cockpit/images is history, this is now only being used for a local S3
    store (on e2e or in integration tests), so move the service setup and
    certificate generation scripts into their own local-s3/ directory, and
    rename the certificates to s3-server.{pem,key}.

--- local-s3/generate-s3-cert.sh
+++ local-s3/generate-s3-cert.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# Generate image server certificate
+# Generate local S3 image server certificate
 set -eux
 
 ROOTDIR="$(realpath -m "$0"/../../)"
 OPENSSL_CNF="$ROOTDIR/tasks/credentials/openssl.cnf"
 
-openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr \
+openssl req -new -newkey rsa:2048 -nodes -keyout s3-server.key -out s3-server.csr \
     -subj '/O=Cockpit/OU=Cockpituous/CN=cockpit-tests' \
     -extensions server_ca_extensions -reqexts server_ca_extensions \
     -config "$OPENSSL_CNF"
-openssl x509 -req -days 365000 -in server.csr -out server.pem \
+openssl x509 -req -days 365000 -in s3-server.csr -out s3-server.pem \
     -CA ../ca.pem -CAkey ../ca.key \
     -set_serial $(date +%s) \
     -extensions server_ca_extensions -extfile "$OPENSSL_CNF"
-rm server.csr
+rm s3-server.csr
