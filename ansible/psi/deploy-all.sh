50a4cfbbda51ca41f01c7f0cda17dd5cae4c318b
Martin Pitt <mpitt@redhat.com>
2024-01-09 08:47:16 +0100

    ansible: Support setting first machine in PSI deploy script
    This is useful for adding more instances after already deployed ones.

--- ansible/psi/deploy-all.sh
+++ ansible/psi/deploy-all.sh
@@ -5,9 +5,12 @@ set -eu
 # cd to ansible/
 cd "$(dirname $(dirname $(realpath $0)))"
 
+# first instance name; can be set by env
+FIRST=${FIRST:-1}
+
 # number of instances; limited by quota
 NUM=35
 
-seq $NUM | parallel --line-buffer -j4 ansible-playbook -i inventory/ -e instance_name='rhos-01-{}' psi/launch-tasks.yml
+seq $FIRST $NUM | parallel --line-buffer -j4 ansible-playbook -i inventory/ -e instance_name='rhos-01-{}' psi/launch-tasks.yml
 
 ansible-playbook -i inventory/ psi/image-cache.yml
